3d9e015ab2f2675a1ab4bb8b5d3f75d83a37d207
wing <jesusblessf6@gmail.com>
2013-10-29 17:22:44 +0800

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
+var index = require('./routes/index');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -30,7 +31,7 @@ if ('development' == app.get('env')) {
 }
 
 
-app.get('/', routes.index);
+app.get('/', index.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  //res.send("respond with a resource");
+  res.render('user/user_list', {title: "hello"});
 };
